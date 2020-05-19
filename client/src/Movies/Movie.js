80a47ca4c8a5f60381baf44ee4067a57b69c407d
maryjwaters7 <60704985+maryjwaters7@users.noreply.github.com>
2020-05-19 12:03:45 -0400

    updated dumpster fire

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
+
 const Movie = (props) => {
   const [movie, setMovie] = useState();
- 
+ console.log(props)
   useEffect(() => {
     const id = 1;
     // change ^^^ that line and grab the id from the URL
